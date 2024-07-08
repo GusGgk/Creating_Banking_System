924567a6269db24e57838d7ff5c65c873df4a212
Gustavo Giacoia Kumagai <165984624+GusGgk@users.noreply.github.com>
2024-07-08 18:01:13 -0300

    Add files via upload

---
+++ 3- Mnipulando Strings com Python/3- Fatiamento de string/string_etapa3.py
@@ -0,0 +1,12 @@
+nome = "Gustavo Giacoia Kumagai"
+
+#FATIAMENTO DAS STRINGS
+print(nome[0])
+print(nome[-1])
+print(nome[:7])
+print(nome[7:])
+print(nome[8:15])
+print(nome[8:15:2])
+print(nome[:])
+#truque de espelhamento de uma string
+print(nome[:: -1])
---
+++ 3- Mnipulando Strings com Python/4- String multiplas linhas/string_etapa4.py
@@ -0,0 +1,26 @@
+#STRING TRIPLA
+
+nome = "Gustavo"
+
+mensagem = f"""
+  Olá meu nome é {nome},
+Eu estou aprendendo Python
+    Essa mensagem tem diferentes recuos.
+"""
+
+print(mensagem)
+
+print("""
+     --------------MENU--------------- 
+      
+
+      1- Depositar
+      2- sacar
+      0- sair
+
+     ---------------------------------
+
+      Obrigado por usar nosso sistema
+      
+    """ )
+
